941e88eedb7aa725038d75e0f636282cc7022d6e
thorfinn12 <luka.chkhitunidze.1@btu.edu.ge>
2022-09-26 12:50:44 +0400

    lesson 3

---
+++ lesson3.1/exercise3.py
@@ -0,0 +1,82 @@
+#------len function
+
+# name = 'luka chkhitunidze'
+
+# print(len(name))
+
+#---long string
+                      #____________________________________________
+# my_sentence = '''hy,  # use """ """ instead of " " for long strings\
+# my                    #____________________________________________/
+# name
+# is
+# luka
+# '''
+# print(my_sentence)
+
+
+#-----know_programming = True
+
+
+# if 20 > 10:
+#     print('ur good one ;)))')
+
+full_name = 'luka chkhitunidze'
+
+#vitvlit wevrebis raodenobas
+#         /
+print(len(full_name))
+
+#am gzit vigebt romelia mexute wevri "luka chkhitunidze"-shi
+#          /
+print(full_name[5])
+
+
+#am gzit shegvidzlia gavigot aris tu ara raime sityva an aso
+
+print("luka" in full_name)  # tu aris iprinteba True
+
+print('luka' not in full_name)
+
+print("malxazi" in full_name) # tu ara False
+
+print(full_name[3:9])  # am metodit shegvidzlia davprintot mesame wevridan anu a dan i mde
+
+print(full_name[2:11]) # igivea
+
+print(full_name[1:10:2]) 
+
+'''am shemtxvevashi sityva iprinteba u dan tmde magram yovel meore wevrs tovebs,anu tu viwyebt uka chkhi dan
+daprintavs msgavsad uacki'''
+
+#lower/upper/replace/strip
+#examples
+
+second_name = 'LUKA CHKHITUNIDZE'
+
+print(second_name.lower())
+
+print(full_name.upper())
+
+# new_str = ""
+# for char in full_name:
+#     if char != " ":
+#         new_str += char
+# print(new_str)
+
+print(full_name.replace("luka","giorgi"))
+
+
+    
+    
+
+
+
+
+    
+
+
+
+
+
+
